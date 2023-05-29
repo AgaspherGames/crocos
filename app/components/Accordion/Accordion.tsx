49833c1ce5545ec1ea2bb61e78c717f57a370b1a
AgaspherGames <agasphergg@gmail.com>
2023-05-29 22:27:22 +0600

    pc

--- app/components/Accordion/Accordion.tsx
+++ app/components/Accordion/Accordion.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import TitleButtons from "../TitleButtons/TitleButtons";
 import { Accordion, AccordionItem } from "@szhsin/react-accordion";
 import FlatButton from "../OutlineButton/FlatButton";
+import ArrowIcon from "@/app/icons/ArrowIcon";
 interface AccordionProps {}
 
 const AccordionSection: React.FC<AccordionProps> = () => {
@@ -11,28 +12,49 @@ const AccordionSection: React.FC<AccordionProps> = () => {
         title="FAQ"
         buttons={<FlatButton outline>Смотреть все</FlatButton>}
       />
-      <Accordion >
-        <AccordionItem header="What is Lorem Ipsum?">
-          <h3>
-            Aliquam
-            </h3>
+      <Accordion>
+        <AccordionItem
+          header={
+            <div className="accordion-header">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+              viverra sed ultricies elit praesent scelerisque. Id enim, est a,
+              aenean cras rhoncus?
+              <ArrowIcon />
+            </div>
+          }
+        >
+          <h3>Aliquam</h3>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </AccordionItem>
 
-        <AccordionItem header="Where does it come from?">
-            <h3>
-            Aliquam
-            </h3>
+        <AccordionItem
+          header={
+            <div className="accordion-header">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+              viverra sed ultricies elit praesent scelerisque. Id enim, est a,
+              aenean cras rhoncus?
+              <ArrowIcon />
+            </div>
+          }
+        >
+          <h3>Aliquam</h3>
           Quisque eget luctus mi, vehicula mollis lorem. Proin fringilla vel
           erat quis sodales. Nam ex enim, eleifend venenatis lectus vitae,
           accumsan auctor mi.
         </AccordionItem>
 
-        <AccordionItem header="Why do we use it?">
-            <h3>
-            Aliquam
-            </h3>
+        <AccordionItem
+          header={
+            <div className="accordion-header">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+              viverra sed ultricies elit praesent scelerisque. Id enim, est a,
+              aenean cras rhoncus?
+              <ArrowIcon />
+            </div>
+          }
+        >
+          <h3>Aliquam</h3>
           Suspendisse massa risus, pretium id interdum in, dictum sit amet ante.
           Fusce vulputate purus sed tempus feugiat.
         </AccordionItem>
